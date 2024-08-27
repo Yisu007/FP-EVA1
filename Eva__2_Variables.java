c5f59858ced6799bee11f7812d9f3daeb05ed01e
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-08-26 21:13:29 -0600

    Add files via upload

---
+++ Eva__2_Variables.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_._2_.variables;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class Eva__2_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here// TODO code application logic here
+                                         //tipos de datos 
+        System.out.println("HOLA MUNDO");//CADENA DE TEXTO 
+        System.out.println(100);//entero 
+      
+        System.out.println(3.1416);//
+        System.out.println('c');
+        System.out.println(true);
+        System.out.println(false);
+        
+     
+        //;fin de introducción
+    
+   //entero
+    //int
+    //long
+    //short
+    
+    //NUMEROS POR PARTE DECIMAL (FLOTANTES):
+    //float
+    //doblue
+    //CADENAS DE TEXTO:
+    //String
+    //CARACTERES:
+    //char
+    //VALORES LOGICOS:
+    //booleans
+    
+    //DECLARACIÓ DE VARIAVLES:
+    //sintaxis para declarar variavles(reglas)
+    //tipo de datos nombre de la variavle;
+    String nombre;
+    String apellido;
+    String curp;
+    double promedio;
+    int edad;
+    
+    edad = 18; //ASIGNACIÓN; estoy colocando el valor 18
+               //dentro de la variable edad 
+    nombre = " RUBEN ALONZO ";        
+    apellido = "HERNANDEZ CHAVEZ";
+    promedio = 1.0; 
+    System.out.println(nombre);
+            
+    System.out.println(apellido);
+    
+    System.out.println(promedio);
+    
+    System.out.println(edad);
+    }
+    
+}
---
+++ eva_!_3VARIAVLES2/src/eva_/_3variavles2/Eva__3VARIAVLES2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_._3variavles2;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class Eva__3VARIAVLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VEHICULOS;
+        String marca ;
+        
+        String modelo ;
+        int year ;// la ñ en otros lenguajes no es aceptada 
+        double precio ;
+        
+        //asignación 
+        marca = "NISSAN";
+        modelo = "GTR";
+        year = 2008;
+        precio = 1000000.1;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+                
+                
+    }
+    
+}
