e6c3f98bc07d7bc1c7b11514cbf00709ae43d80b
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-09-04 00:16:03 -0600

    Add files via upload

---
+++ EVA_9_operaciones/src/eva_9_operaciones/EVA_9_operaciones.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_9_operaciones;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class EVA_9_operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        // operacion aridmetica
+        // inicializacion de una variable 
+        int x = 10; //DECLARACION Y ASIGNACION 
+        int y = 5;
+        // si necesitan muchas variables 
+        int suma,multi,divot, raiz,resta;
+        // suma y resta ¨*
+        suma= x + y; //operacion de suma 
+        System.out.print(" la suma de x+y");
+        System.out.print(suma);
+        
+        
+       //resta 
+       resta= x-y;
+        System.out.println("la resta de x-y");
+         System.out.print(resta);
+         
+         multi= x+y; //operacion de multiplicasion
+          System.out.println("la pultiplicacion de x+y");
+           System.out.print(multi);
+           
+       
+       
+        
+ 
+        
+        
+        
+       
+        
+        
+        
+     
+          
+    }
+    
+}
