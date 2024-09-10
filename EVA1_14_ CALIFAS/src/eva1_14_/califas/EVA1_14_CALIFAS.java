b615406d4a6d1ff37e76bbaad8b57d3689f8b953
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-09-10 17:32:33 -0600

    Add files via upload

---
+++ EVA1_14_ CALIFAS/src/eva1_14_/califas/EVA1_14_CALIFAS.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_14_.califas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class EVA1_14_CALIFAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cali;
+       Scanner captu = new Scanner (System.in);
+       System.out.println("captura la calificacion");
+       cali=captu.nextInt();
+       if(cali<70){//se puede poner if (cali >=70)
+       System.out.println("calificacion: no aprobada ");
+       
+         }else{
+          System.out.println("calificacion  aprobatoria ");
+       
+       
+       
+       
+    }
+    
+
+    }}
---
+++ EVA1_15_AGUILA_SELLO/src/eva1_15_aguila_sello/EVA1_15_AGUILA_SELLO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_aguila_sello;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class EVA1_15_AGUILA_SELLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //0.0---0.9999999999999999
+        //0.5
+        //0 1 2 3 4 5 6 7 8 9 
+        double valor = Math.random();
+        if(valor< 0.5){
+            System.out.println("aguila ");
+        }else{
+            System.out.println("sello");
+        }
+                
+        
+       
+        
+        
+    }
+    
+}
